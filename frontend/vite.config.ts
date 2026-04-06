8b2e9a06cc6c4559b3ec6d568d999d06744bb1aa
shaswattrivedi <shaswattriv@gmail.com>
2026-04-06 14:58:05 +0530

    TypeScript Migration to Vite Based Setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
+      "@": path.resolve(__dirname, "src"),
+      "@/api": path.resolve(__dirname, "src/api"),
+      "@/context": path.resolve(__dirname, "src/context"),
+      "@/components": path.resolve(__dirname, "src/components"),
+      "@/pages": path.resolve(__dirname, "src/pages")
     }
   },
   server: {
